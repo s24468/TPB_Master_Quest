b91a2cca65b8d6b75fb5c78a59ef544c75bcc646
Jarek <s24468@pjwstk.edu.pl>
2024-12-10 03:06:35 +0100

    hover over sprites

--- Assets/Scripts/UI/HoverOver.cs
+++ Assets/Scripts/UI/HoverOver.cs
@@ -1,23 +1,36 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class HoverOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public Light pointLeft;
-    public Light pointRight;
+    public Sprite newSprite;
+    public Sprite originalSprite;
+    public Image buttonImage;
+
+    private Light[] _lights;
+
+    void Start()
+    {
+        _lights = GetComponentsInChildren<Light>();
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        
-        pointLeft.color =  new Color(255f / 255f, 22f / 255f, 0f / 255f, 255f / 255f);
-        pointRight.color =  new Color(255f / 255f, 22f / 255f, 0f / 255f, 255f / 255f);
-
+        buttonImage.sprite = newSprite;
+        foreach (var light in _lights)
+        {
+            light.color = new Color(255f / 255f, 22f / 255f, 0f / 255f, 255f / 255f);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        pointLeft.color =  new Color(22f / 255f, 182f / 255f, 177f / 255f, 255f / 255f);
-        pointRight.color =  new Color(22f / 255f, 182f / 255f, 177f / 255f, 255f / 255f);
+        buttonImage.sprite = originalSprite;
+        foreach (var light in _lights)
+        {
+            light.color = new Color(22f / 255f, 182f / 255f, 177f / 255f, 255f / 255f);
+        }
     }
 }
