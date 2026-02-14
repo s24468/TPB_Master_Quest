e943fd61d9227d8d967317d2fa25af2915ffac8f
Jarek <s24468@pjwstk.edu.pl>
2026-02-14 16:35:35 +0100

    deleted dragging scripts

--- Assets/Scripts/Dragging/DragCreatureAttack.cs
+++ Assets/Scripts/Dragging/DragCreatureAttack.cs
@@ -1,42 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
 using Cards;
 using Dragging;
 
 public class DragCreatureAttack : DraggingActions
 {
-    // reference to the sprite with a round "Target" graphic
-    private SpriteRenderer sr;
+    private SpriteRenderer _sr;
 
-    // LineRenderer that is attached to a child game object to draw the arrow
-    private LineRenderer lr;
+    private LineRenderer _lr;
 
-    // reference to WhereIsTheCardOrCreature to track this object`s state in the game
-    private WhereIsTheCardOrCreature whereIsThisCreature;
+    private WhereIsTheCardOrCreature _whereIsThisCreature;
 
-    // the pointy end of the arrow, should be called "Triangle" in the Hierarchy
-    private Transform triangle;
+    private Transform _triangle;
 
-    // SpriteRenderer of triangle. We need this to disable the pointy end if the target is too close.
-    private SpriteRenderer triangleSR;
+    private SpriteRenderer _triangleSr;
 
-    // when we stop dragging, the gameObject that we were targeting will be stored in this variable.
-    private GameObject Target;
+    private GameObject _target;
 
-    // Reference to creature manager, attached to the parent game object
-    private OneCreatureManager manager;
+    private OneCreatureManager _manager;
 
     void Awake()
     {
-        // establish all the connections
-        sr = GetComponent<SpriteRenderer>();
-        lr = GetComponentInChildren<LineRenderer>();
-        lr.sortingLayerName = "AboveEverything";
-        triangle = transform.Find("Triangle");
-        triangleSR = triangle.GetComponent<SpriteRenderer>();
+        _sr = GetComponent<SpriteRenderer>();
+        _lr = GetComponentInChildren<LineRenderer>();
+        _lr.sortingLayerName = "AboveEverything";
+        _triangle = transform.Find("Triangle");
+        _triangleSr = _triangle.GetComponent<SpriteRenderer>();
 
-        manager = GetComponentInParent<OneCreatureManager>();
-        whereIsThisCreature = GetComponentInParent<WhereIsTheCardOrCreature>();
+        _manager = GetComponentInParent<OneCreatureManager>();
+        _whereIsThisCreature = GetComponentInParent<WhereIsTheCardOrCreature>();
     }
 
     public override bool CanDrag
@@ -46,17 +37,15 @@ public class DragCreatureAttack : DraggingActions
             // we can drag this card if 
             // a) we can control this our player (this is checked in base.canDrag)
             // b) creature "CanAttackNow" - this info comes from logic part of our code into each creature`s manager script
-            return base.CanDrag && manager.CanAttackNow;
+            return base.CanDrag && _manager.CanAttackNow;
         }
     }
 
     public override void OnStartDrag()
     {
-        whereIsThisCreature.VisualState = VisualStates.Dragging;
-        // enable target graphic
-        sr.enabled = true;
-        // enable line renderer to start drawing the line.
-        lr.enabled = true;
+        _whereIsThisCreature.VisualState = VisualStates.Dragging;
+        _sr.enabled = true;
+        _lr.enabled = true;
     }
 
     public override void OnDraggingInUpdate()
@@ -67,25 +56,25 @@ public class DragCreatureAttack : DraggingActions
         if (notNormalized.magnitude > distanceToTarget)
         {
             // draw a line between the creature and the target
-            lr.SetPositions(new Vector3[] { transform.parent.position, transform.position - direction * 2.3f });
-            lr.enabled = true;
+            _lr.SetPositions(new Vector3[] { transform.parent.position, transform.position - direction * 2.3f });
+            _lr.enabled = true;
 
             // position the end of the arrow between near the target.
-            triangleSR.enabled = true;
-            triangleSR.transform.position = transform.position - 1.5f * direction;
+            _triangleSr.enabled = true;
+            _triangleSr.transform.position = transform.position - 1.5f * direction;
 
             // proper rotarion of arrow end
             float rot_z = Mathf.Atan2(notNormalized.y, notNormalized.x) * Mathf.Rad2Deg;
-            triangleSR.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+            _triangleSr.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
         }
         else
         {
-            // if the target is not far enough from creat
-            // re, do not show the arrow
-            lr.enabled = false;
-            triangleSR.enabled = false;
+            // if the target is not far enough from creature, do not show the arrow
+            _lr.enabled = false;
+            _triangleSr.enabled = false;
         }
     }
+
     public override void OnEndDrag()
     {
         Debug.Log("End dragging");
@@ -114,13 +103,6 @@ public class DragCreatureAttack : DraggingActions
                 continue;
             }
 
-            // ignoruj własne obiekty
-            // var myOwner = manager.owner; // jeśli masz owner w managerze
-            // if (candidate.Owner == myOwner)
-            // {
-            //     continue;
-            // }
-            // jeśli nie masz, to weź z CreatureLogic (poniżej wariant)
             var attackerId = GetComponentInParent<IDHolder>().UniqueID;
             var attackerOwnerId = CreatureLogic.CreaturesCreatedThisGame[attackerId].owner.PlayerID;
 
@@ -141,7 +123,8 @@ public class DragCreatureAttack : DraggingActions
             var attackerHolder = GetComponentInParent<IDHolder>();
             string attackerId = attackerHolder != null ? attackerHolder.UniqueID : string.Empty;
 
-            Debug.Log($"Target picked: {(targetAttackable as MonoBehaviour)?.name}, targetID: {targetAttackable.UniqueID}");
+            Debug.Log(
+                $"Target picked: {(targetAttackable as MonoBehaviour)?.name}, targetID: {targetAttackable.UniqueID}");
 
             targetAttackable.ReceiveAttack(attackerId);
             targetValid = true;
@@ -150,87 +133,16 @@ public class DragCreatureAttack : DraggingActions
         // 3) jeśli nieważny target -> wróć
         if (!targetValid)
         {
-            whereIsThisCreature.VisualState = VisualStates.LowTable;
-            whereIsThisCreature.SetTableSortingOrder();
+            _whereIsThisCreature.VisualState = VisualStates.LowTable;
+            _whereIsThisCreature.SetTableSortingOrder();
         }
 
         // 4) zawsze resetuj wizual
         transform.localPosition = Vector3.zero;
-        sr.enabled = false;
-        lr.enabled = false;
-        triangleSR.enabled = false;
+        _sr.enabled = false;
+        _lr.enabled = false;
+        _triangleSr.enabled = false;
     }
-
-    // public override void OnEndDrag()
-    // {
-    //     Debug.Log("End dragging");
-    //
-    //     // 1) szukamy targetu
-    //     Target = null;
-    //
-    //     var origin = Camera.main.transform.position;
-    //     var dir = (transform.position - origin).normalized;
-    //
-    //     RaycastHit[] hits = Physics.RaycastAll(origin, dir, 200f);
-    //
-    //     // (opcjonalnie) dla debug:
-    //     Debug.Log($"Raycast hits: {hits.Length}");
-    //
-    //     foreach (var h in hits)
-    //     {
-    //         // ignoruj samego siebie i swoje dzieci
-    //         if (h.transform == transform) continue;
-    //         // ignoruj wszystko bez holdera
-    //         if (h.transform.GetComponentInParent<IDHolder>() == null)
-    //             continue;
-    //         CreatureLogic target =
-    //             CreatureLogic.CreaturesCreatedThisGame[h.transform.GetComponentInParent<IDHolder>().UniqueID];
-    //         if (target.owner.PlayerID == CreatureLogic.CreaturesCreatedThisGame[GetComponentInParent<IDHolder>().UniqueID].owner.PlayerID)
-    //             continue;
-    //
-    //         //TODO ogarnąć, że to przeciwnika poprzez lower card i top cards
-    //         Target = h.transform.gameObject;
-    //         break;
-    //     }
-    //
-    //     bool targetValid = false;
-    //
-    //     // 2) walidacja targetu i wykonanie akcji
-    //     if (Target != null)
-    //     {
-    //         var idHolder = Target.GetComponentInParent<IDHolder>();
-    //         if (idHolder != null)
-    //         {
-    //             string targetID = idHolder.UniqueID;
-    //             Debug.Log($"Target: {Target.name}, targetID: {targetID}");
-    //
-    //             if (CreatureLogic.CreaturesCreatedThisGame.TryGetValue(targetID, out var creature) &&
-    //                 creature != null)
-    //             {
-    //                 CreatureLogic.CreaturesCreatedThisGame[GetComponentInParent<IDHolder>().UniqueID]
-    //                     .AttackCreatureWithID(targetID);
-    //                 targetValid = true;
-    //             }
-    //         }
-    //         else
-    //         {
-    //             Debug.Log($"Target {Target.name} has failed somehow");
-    //         }
-    //     }
-    //
-    //     // 3) jeśli nieważny target -> wróć
-    //     if (!targetValid)
-    //     {
-    //         whereIsThisCreature.VisualState = VisualStates.LowTable;
-    //         whereIsThisCreature.SetTableSortingOrder();
-    //     }
-    //
-    //     // 4) zawsze resetuj wizual
-    //     transform.localPosition = Vector3.zero;
-    //     sr.enabled = false;
-    //     lr.enabled = false;
-    //     triangleSR.enabled = false;
-    // }
 
     // NOT USED IN THIS SCRIPT
     protected override bool DragSuccessful()
